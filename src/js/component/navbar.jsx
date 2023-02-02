699fad53e4925a6368699c560e1e914079d3f743
yisas93 <juan249325@gmail.com>
2023-02-02 23:04:36 +0000

    finished

--- src/js/component/navbar.jsx
+++ src/js/component/navbar.jsx
@@ -6,31 +6,31 @@ export const Navbar = () => {
 	const {actions, store}=useContext(Context)
 	
 	return (
-		<nav class="navbar navbar-expand-lg navbar-light bg-black wholenav">
-		<div class="container-fluid">
-		  <Link class="navbar-brand" style={{color: "green"}} to="/">Home</Link>
-		  <button style={{backgroundColor: "green"}} class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"  aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-			<span  class="navbar-toggler-icon"></span>
+		<nav className="navbar navbar-expand-lg navbar-light bg-black wholenav">
+		<div className="container-fluid">
+		  <Link className="navbar-brand" style={{color: "green"}} to="/">Home</Link>
+		  <button style={{backgroundColor: "green"}} className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"  aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+			<span  className="navbar-toggler-icon"></span>
 		  </button>
-		  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-			<ul class="navbar-nav me-auto mb-2 mb-lg-0">
-			  <li class="nav-item">
-				<Link class="nav-link" style={{color: "green"}} to="/planets">Planets</Link>
+		  <div className="collapse navbar-collapse" id="navbarSupportedContent">
+			<ul className="navbar-nav me-auto mb-2 mb-lg-0">
+			  <li className="nav-item">
+				<Link className="nav-link" style={{color: "green"}} to="/planets">Planets</Link>
 			  </li>
-			  <li class="nav-item">
-				<Link class="nav-link" style={{color: "green"}} to="/characters">Characters</Link>
+			  <li className="nav-item">
+				<Link className="nav-link" style={{color: "green"}} to="/characters">Characters</Link>
 			  </li>
-			  <li class="nav-item">
-				<Link class="nav-link" style={{color: "green"}} to="/films">Films</Link>
+			  <li className="nav-item">
+				<Link className="nav-link" style={{color: "green"}} to="/films">Films</Link>
 			  </li>
-			  <li class="nav-item">
-				<Link class="nav-link" style={{color: "green"}} to="/starship">Starships</Link>
+			  <li className="nav-item">
+				<Link className="nav-link" style={{color: "green"}} to="/starship">Starships</Link>
 			  </li>
-			  <li class="nav-item dropdown">
-				<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" style={{color: "green"}} role="button" data-bs-toggle="dropdown" aria-expanded="false">
+			  <li className="nav-item dropdown">
+				<a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" style={{color: "green"}} role="button" data-bs-toggle="dropdown" aria-expanded="false">
 				  Favorites{store.favorites.length}
 				</a>
-				<ul class="dropdown-menu" aria-labelledby="navbarDropdown" style={{color: "green", backgroundColor: "black"}}>
+				<ul className="dropdown-menu" aria-labelledby="navbarDropdown" style={{color: "green", backgroundColor: "black"}}>
 					{store.favorites.map(favorite=>(
 					<div className="d-flex">	<li className="dropdown-item" style={{color: "green", backgroundColor: "black"}} key={favorite.index}>{favorite.name}</li>
 					<button className="buttfav" onClick={()=>actions.removeFavorites(favorite.index)}>Eliminar</button></div>
